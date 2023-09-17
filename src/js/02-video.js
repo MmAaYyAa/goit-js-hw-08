cbbac8d387e86c55433f94d05510633b4324019a
MmAaYyAa <132580599+MmAaYyAa@users.noreply.github.com>
2023-09-17 19:40:33 +0100

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+// player.on('play', function() {
+//   console.log('played the video!');
+// });
+
+const localStorageKey = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(event => {
+  localStorage.setItem(localStorageKey, event.seconds);
+}, 1000));
+
+
+const savedTime = localStorage.getItem(localStorageKey);
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
+
 
