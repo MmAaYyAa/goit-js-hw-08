df3a20569e294afa1394823e0df05dbfe6896cff
MmAaYyAa <132580599+MmAaYyAa@users.noreply.github.com>
2023-09-18 13:25:47 +0100

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
 
+
+const form = document.querySelector('.feedback-form');
+//console.log(form);
+const localStorageKey = 'feedback-form-state';
+
+form.addEventListener('input', onFormInput);
+
+function onFormInput(event) {
+  const feedback = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+    localStorage.setItem(localStorageKey, JSON.stringify(feedback));
+    console.log(feedback)
+}
+
+let savedFeedback = localStorage.getItem(localStorageKey);
+if (savedFeedback) {
+    savedFeedback = JSON.parse(savedFeedback);
+    form.elements.email.value = savedFeedback.email;
+    form.elements.message.value = savedFeedback.message;
+} else {
+    form.elements.email.value = '';
+    form.elements.message.value = '';
+};
+
+form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    console.log({
+        email: form.elements.email.value,
+        message: form.elements.message.value,
+    });
+
+    form.reset();
+  localStorage.removeItem(localStorageKey);
+};
+
+ 
+
